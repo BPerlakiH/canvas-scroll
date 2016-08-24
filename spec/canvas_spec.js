fc85fcce1f93b18607dc49ce98c3f91458e3fbf3
BPH <ba.perlaki@gmail.com>
2016-08-24 20:58:41 +0200

    configurable

---
+++ spec/canvas_spec.js
@@ -0,0 +1,11 @@
+"use strict";
+
+	describe("Canvas scroll suite", () => {
+
+		it("should find the class", () => {
+			expect(true).toBe(true);
+			const scroller = new CanvasScroll();
+		});
+
+	});
+
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// ( () => {
+	const scroll = new canvas_scroll();
+	scroll.init(
+		{
+			thumbSize: function() { return 100; },
+			margin: function() { return 100; }
+		});
+// })();
